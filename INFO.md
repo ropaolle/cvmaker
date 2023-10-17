fb61ddcd5f03f2bfa922ec5c23334c82c2d048fe
ropaolle <ropaolle@gmail.com>
2023-10-17 17:54:53 +0200

    tailwindcss

--- INFO.md
+++ INFO.md
@@ -8,4 +8,18 @@
 
 ```sh
 pnpm create vite cvmaker --template react-ts
+# Add Tailwind
+pnpm install -D tailwindcss postcss autoprefixer
+pnpm dlx tailwindcss init -p
+
+```
+
+## .vscode/settings.json
+
+```json
+{
+  "files.associations": {
+    "*.css": "tailwindcss"
+  }
+}
 ```
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Olle</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col h-full">
+      <header className="bg-slate-400">Header</header>
+      <main className=" container-fluid flex-1 p-4">Content</main>
+      <footer className=" bg-slate-500">Footer</footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
